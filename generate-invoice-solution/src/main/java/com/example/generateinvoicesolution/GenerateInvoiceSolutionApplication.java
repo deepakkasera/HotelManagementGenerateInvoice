ac6866a63d19b6fe6c5728f626e68bd0b57cb672
deepakkasera <deepak.kasera143.dk@gmail.com>
2024-01-12 00:20:35 +0530

    Generate Invoice

---
+++ generate-invoice-solution/src/main/java/com/example/generateinvoicesolution/GenerateInvoiceSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.example.generateinvoicesolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateInvoiceSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenerateInvoiceSolutionApplication.class, args);
+    }
+
+}
